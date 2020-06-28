30a4e15c9dd631df70d3e4e1f0da256d4a2caa1d
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-06-28 22:04:40 +0200

    Initial commit

---
+++ src/test/java/tech/nautilus/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nautilus.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
