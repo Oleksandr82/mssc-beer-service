b0ecd8d689fd2ff0e75fc8ab6b120223ae2af19c
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-06-29 10:03:53 +0200

    Adding a beer controller

---
+++ src/main/java/tech/nautilus/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,34 @@
+package tech.nautilus.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.nautilus.msscbeerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+
+        // TODO: Implement it
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveNewBeer(@RequestBody BeerDto beerDto) {
+
+        // TODO: implement it
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+
+        // TODO: implement it
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
