fc2464e8a046ba870cbb7dc17d457d93ad1c3a2e
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-07-19 18:53:03 +0200

    Added a mapper decorator to provide additional functionality on top of standard mappings (set quantityOnHand retrieved from the beer-inventory-service)

--- src/main/java/tech/nautilus/msscbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/tech/nautilus/msscbeerservice/web/mappers/BeerMapper.java
@@ -1,20 +1,26 @@
 package tech.nautilus.msscbeerservice.web.mappers;
 
 
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import tech.nautilus.msscbeerservice.domain.Beer;
 import tech.nautilus.brewery.model.BeerDto;
 
 @Mapper(uses = {DateMapper.class})
+@DecoratedWith(BeerMapperDecorator.class)
 public interface BeerMapper {
 
     @Mapping(target = "quantityOnHand", ignore = true)
     BeerDto beerToBeerDto(Beer beer);
 
-    public BeerDto beerToBeerDtoWithInventory(Beer beer);
+//    @Mapping(target = "quantityOnHand", ignore = true)
+    BeerDto beerToBeerDtoWithInventory(Beer beer);
 
-    @Mapping(target = "minOnHand", ignore = true)
-    @Mapping(target = "quantityToBrew", ignore = true)
+    @Mappings({
+            @Mapping(target = "minOnHand", ignore = true),
+            @Mapping(target = "quantityToBrew", ignore = true)
+    })
     Beer beerDtoToBeer(BeerDto dto);
 }
--- src/main/java/tech/nautilus/msscbeerservice/web/mappers/BeerMapperDecorator.java
+++ src/main/java/tech/nautilus/msscbeerservice/web/mappers/BeerMapperDecorator.java
@@ -1,5 +1,6 @@
 package tech.nautilus.msscbeerservice.web.mappers;
 
+import lombok.Setter;
 import tech.nautilus.brewery.model.BeerDto;
 import tech.nautilus.msscbeerservice.domain.Beer;
 import tech.nautilus.msscbeerservice.services.inventory.BeerInventoryService;
@@ -7,23 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 public abstract class BeerMapperDecorator implements BeerMapper {
+
+    @Setter
     private BeerInventoryService beerInventoryService;
+
+    @Setter
     private BeerMapper mapper;
 
-    @Autowired
-    public void setBeerInventoryService(BeerInventoryService beerInventoryService) {
-        this.beerInventoryService = beerInventoryService;
-    }
 
-    @Autowired
-    public void setMapper(BeerMapper mapper) {
-        this.mapper = mapper;
-    }
-
-    @Override
-    public BeerDto beerToBeerDto(Beer beer) {
-        return mapper.beerToBeerDto(beer);
-    }
+//    @Override
+//    public BeerDto beerToBeerDto(Beer beer) {
+//        return mapper.beerToBeerDto(beer);
+//    }
 
     @Override
     public BeerDto beerToBeerDtoWithInventory(Beer beer) {
@@ -32,8 +28,8 @@ public abstract class BeerMapperDecorator implements BeerMapper {
         return dto;
     }
 
-    @Override
-    public Beer beerDtoToBeer(BeerDto beerDto) {
-        return mapper.beerDtoToBeer(beerDto);
-    }
+//    @Override
+//    public Beer beerDtoToBeer(BeerDto beerDto) {
+//        return mapper.beerDtoToBeer(beerDto);
+//    }
 }
