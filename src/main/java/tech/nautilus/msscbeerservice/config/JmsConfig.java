8b7ce48546d75eac5cf8fa2c2e1ba2070bebbb70
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-07-25 21:45:28 +0200

    Preconfigured projects to use JMS with Artemis

---
+++ src/main/java/tech/nautilus/msscbeerservice/config/JmsConfig.java
@@ -0,0 +1,21 @@
+package tech.nautilus.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public final static String TYPE_PROP_NAME = "_type";
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName(TYPE_PROP_NAME);
+        return converter;
+    }
+}
