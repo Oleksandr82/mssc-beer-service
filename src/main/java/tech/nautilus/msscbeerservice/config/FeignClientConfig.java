48dad933709bffac651e827d6e16949d66d3e545
Oleksandr Sytnik <alexandr.sytnik@gmail.com>
2020-08-23 12:33:28 +0200

    Added Basic Authentication to the Inventory Service (and updated REST Client and Feign Client in the Beer Service to access the Inventory Service).

---
+++ src/main/java/tech/nautilus/msscbeerservice/config/FeignClientConfig.java
@@ -0,0 +1,20 @@
+package tech.nautilus.msscbeerservice.config;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local-discovery")
+public class FeignClientConfig {
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor(
+            @Value("${sfg.brewery.inventory-user}") String inventoryUser,
+            @Value("${sfg.brewery.inventory-password}") String inventoryPassword) {
+
+        return new BasicAuthRequestInterceptor(inventoryUser, inventoryPassword);
+    }
+}
--- src/main/java/tech/nautilus/msscbeerservice/services/inventory/BeerInventoryServiceFeignClient.java
+++ src/main/java/tech/nautilus/msscbeerservice/services/inventory/BeerInventoryServiceFeignClient.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import tech.nautilus.msscbeerservice.config.FeignClientConfig;
 import tech.nautilus.msscbeerservice.services.inventory.model.BeerInventoryDto;
 
 import java.util.List;
@@ -12,7 +13,11 @@ import java.util.UUID;
 
 import static tech.nautilus.msscbeerservice.services.inventory.BeerInventoryService.INVENTORY_PATH;
 
-@FeignClient(name = "inventory-service", fallback = BeerInventoryServiceFailoverFeignClientImpl.class)
+@FeignClient(
+        name = "inventory-service",
+        fallback = BeerInventoryServiceFailoverFeignClientImpl.class,
+        configuration = FeignClientConfig.class
+)
 public interface BeerInventoryServiceFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = INVENTORY_PATH)
